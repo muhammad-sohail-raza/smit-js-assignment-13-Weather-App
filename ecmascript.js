fd6fc067a5198c05f813370000843c114848d0e7
muhammad-sohail-raza <sohail.raza.webdev@gmail.com>
2026-09-23 00:39:21 +0500

    smit assignment

--- ecmascript.js
+++ ecmascript.js
@@ -5,12 +5,15 @@ let para4 = document.querySelector(".show-heat");
 let para5 = document.querySelector(".show-cloud");
 let para6 = document.querySelector(".show-country");
 let para7 = document.querySelector(".show-city");
+let icon1 = document.querySelector(".fa-solid fa-location-dot");
+let icon2 = document.querySelector(".dot");
+let tittle = document.querySelector(".h1");
 
 async function getweather(event) {
   event.preventDefault();
 
-  const city = document.querySelector("#inp").value;
-
+  let city = document.querySelector("#inp").value.trim();
+  city = city.charAt(0).toUpperCase() + city.slice(1);
   const apidata = await axios(
     `https://api.weatherapi.com/v1/current.json?key=335cb659d26e4e7e810152041261309&q=${city}`,
   );
@@ -19,30 +22,34 @@ async function getweather(event) {
   document.querySelector(".weather-container").style.display = "grid";
 
   //   Weather condition check karna
-  // const condition = apidata.data.current.condition.text.toLowerCase();
+  const condition = apidata.data.current.condition.text.toLowerCase();
+  document.body.style.animation = "none";
+  if (condition.includes("sunny")) {
+    document.body.style.backgroundImage = "url('./assets/image2.jpg')";
+    tittle.style.color = "white";
+    tittle.style.textshadow = "0 3px 8px rgba(0, 0, 0, 0.4)";
+  } else if (condition.includes("cloud")) {
+    document.body.style.backgroundImage = "url('./assets/image1.jpg')";
+  } else if (condition.includes("rain")) {
+    document.body.style.backgroundImage = "url('./assets/image3.jpg')";
+  } else if (
+    apidata.data.current.is_day == 0 &&
+    condition.includes("rain") !== "rain"
+  ) {
+    document.body.style.backgroundImage = "url('./assets/image4.jpg')";
+    tittle.style.color = "white";
+    tittle.style.textshadow = "0 3px 8px rgba(0, 0, 0, 0.4)";
+  } else {
+    document.body.style.backgroundImage = "url('./assets/image5.png')";
+  }
 
-  // if (condition.includes("sunny") || condition.includes("clear")) {
-  //   document.body.style.backgroundImage = "url('./assets/image1.jpg')";
-  // }
-  // else if (condition.includes("cloud")) {
-  //   document.body.style.backgroundImage = "url('./assets/image2.jpg')";
-  // }
-  // else if (condition.includes("rain")) {
-  //   document.body.style.backgroundImage = "url('rainy.jpg')";
-  // }
-  // else if (condition.includes("snow")) {
-  //   document.body.style.backgroundImage = "url('snow.jpg')";
-  // }
-  // else {
-  //   document.body.style.backgroundImage = "url('default.jpg')";
-  // }
-
+  document.body.style.animation = "fadeBackground 1s ease";
   para1.innerHTML = "Weather: " + apidata.data.current.temp_c;
   para2.innerHTML = "humidity: " + apidata.data.current.humidity + "%";
   para3.innerHTML =
     "chance of rain: " + apidata.data.current.chance_of_rain + "%";
   para4.innerHTML = "Heat: " + apidata.data.current.heatindex_c + "°C";
   para5.innerHTML = "Cloud: " + apidata.data.current.cloud + "%";
-  para6.innerHTML += "Country: " + apidata.data.location.country;
-  para7.innerHTML += "City: " + apidata.data.location.name;
+  para6.innerHTML = `<i class="fa-solid fa-flag" class="flag"></i> Country: ${apidata.data.location.country}`;
+  para7.innerHTML = `<i class="fa-solid fa-location-dot"></i> City / Province: ${apidata.data.location.name} ${apidata.data.location.region}`;
 }
